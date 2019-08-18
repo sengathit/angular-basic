5c4d69599dad710d42834d477bd689bc049b495b
sengathit <sengathit.l@gmail.com>
2019-08-17 20:30:59 -0500

    changes

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist/lavanh-hello-world'))
+app.use(express.static(__dirname + '/dist'))
 
 const port = process.env.PORT || 3000
 
 app.get('',(req,res) => {
-    res.sendFile(path.join(__dirname + '/dist/lavanh-hello-world/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
 
