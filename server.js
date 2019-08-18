2e7355dacd129f3abf0c1ca43f0aa7de6fbb8353
sengathit <sengathit.l@gmail.com>
2019-08-17 22:23:10 -0500

    adding correct version

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist/hello-world'))
+app.use(express.static(__dirname + '/dist'))
 
 const port = process.env.PORT || 3000
 
-app.get('',(req,res) => {
-    res.sendFile(path.join(__dirname + '/dist/hello-world/index.html'))
+app.get('/*',(req,res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
 
