dbd502fc6882c3eba17f81773c8ff04deb3350ec
sengathit <sengathit.l@gmail.com>
2019-08-17 19:34:05 -0500

    auto deploy from heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('',(req,res) => {
-    res.send('HEY MR. DJ')
+    res.send('GET IT')
 })
 
 app.listen(port,() => console.log('Server started on : ' + port))
