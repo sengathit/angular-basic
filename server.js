e608f5bdb4c48323cd9e269948fb027da23ce15b
sengathit <sengathit.l@gmail.com>
2019-08-17 19:44:26 -0500

    deploy angular to heroku

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(__dirname + '/dist/lavanh-hello-world'))
 
 const port = process.env.PORT || 3000
 
 app.get('',(req,res) => {
-    res.send('GET IT!')
+    res.sendFile(path.join(__dirname + '/dist/lavanh-hello-world/index.html'))
 })
+
 
 app.listen(port,() => console.log('Server started on : ' + port))
